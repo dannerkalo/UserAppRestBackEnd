494a80c7d5be1e9fb3e345b748c58e2802d90313
Dannerkalo <Dannerkalo@gmail.com>
2019-12-23 14:58:59 +0100

    initial project version

---
+++ src/main/java/com/userapprest/demo/UserAppRestApplication.java
@@ -0,0 +1,13 @@
+package com.userapprest.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserAppRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserAppRestApplication.class, args);
+	}
+
+}
