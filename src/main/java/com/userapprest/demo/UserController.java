25304ea2f165a484bd0c763c0b66f277ba950580
Dannerkalo <Dannerkalo@gmail.com>
2019-12-23 15:58:03 +0100

    Unused imports are removed and code indentation

--- src/main/java/com/userapprest/demo/UserController.java
+++ src/main/java/com/userapprest/demo/UserController.java
@@ -1,11 +1,9 @@
 package com.userapprest.demo;
 	
-	import java.util.ArrayList;
+import java.util.ArrayList;
 import java.util.List;
-
 import org.springframework.web.bind.annotation.RequestMapping;
-	import org.springframework.web.bind.annotation.RequestParam;
-	import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestController;
 
 	@RestController
 	public class UserController {
